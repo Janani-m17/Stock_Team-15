a8face8936078ae029fa3e7884fad8c46af6055d
Janani <janani.m2022cce@sce.ac.in>
2023-12-07 10:57:17 +0530

    signup redirect commit

--- src/main/java/com/example/stock_team15/HelloApplication.java
+++ src/main/java/com/example/stock_team15/HelloApplication.java
@@ -12,12 +12,18 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 665, 476);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
+
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 665, 476));
+        primaryStage.show();
+
     }
 
     public static void main(String[] args) {
